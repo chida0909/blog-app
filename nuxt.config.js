1e36d436f3aea4379c1b5d932b1a74e3561bd048
千田孝明 <t_chida@codmon.co.jp>
2020-04-04 23:13:43 +0900

    test

--- nuxt.config.js
+++ nuxt.config.js
@@ -86,9 +86,11 @@ export default {
   },
   generate: {
     routes() {
-      const jsonData = require('./posts/summary.json')
-      const postDates = jsonData.sourceFileArray.map( s => s.replace(/[^0-9]/g, '') )
-      const contents = postDates.map( p => fileMap[`posts/json/${p}.json`] )
+      // const jsonData = require('./posts/summary.json')
+      const jsonData = [ '20200328', '20200329' ]
+      // const postDates = jsonData.map( s => s.replace(/[^0-9]/g, '') )
+      // const postDates = jsonData.sourceFileArray.map( s => s.replace(/[^0-9]/g, '') )
+      const contents = jsonData.map( p => fileMap[`posts/json/${p}.json`] )
       return contents.map( p => p.permalink )
     }
   }
