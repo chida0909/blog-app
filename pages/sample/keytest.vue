7b017eeb6f432cf6dbb99c97b2730cf1dcaa70f6
千田孝明 <t_chida@codmon.co.jp>
2020-04-07 23:57:37 +0900

    記事更新とサンプルページの作成

---
+++ pages/sample/keytest.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-app>
+    <div v-for="(user, index) in users" :key="index">
+      <v-btn @click="userDelete(index)" style="margin: 8px;" >{{index}}: {{user.name}}</v-btn>
+    </div>
+  </v-app>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      users: []
+    }
+  },
+  mounted() {
+    axios
+      .get('https://jsonplaceholder.typicode.com/users')
+      .then(response => (this.users = response.data))
+  },
+  methods: {
+    userDelete(index) {
+      this.$delete(this.users, index)
+      console.log(index)
+    }
+  }
+}
+</script>
+
